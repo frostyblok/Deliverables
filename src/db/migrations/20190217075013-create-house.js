bfd5cc86e4a5d34c6010cd934c7ce6922c642e76
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-02-17 09:33:20 +0100

    (chore) Setup base application for project - Create models and run migrations - Install neccessary packages to run application

---
+++ src/db/migrations/20190217075013-create-house.js
@@ -0,0 +1,38 @@
+export default {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Houses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      item_name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      imgUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => { queryInterface.dropTable('Houses');
+  }
+};
